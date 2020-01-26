3b884f02564d90906722f81e86d898da2b4191a8
kaungmaw <kaungmawaung99@gmail.com>
2020-01-26 09:22:33 +0630

    first commit

---
+++ src/main/java/com/example/springmvconlineshop/SpringMvcOnlineShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.springmvconlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcOnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcOnlineShopApplication.class, args);
+    }
+}
